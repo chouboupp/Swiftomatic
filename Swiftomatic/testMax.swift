bbe50dd0f6e065f95a28c5080921b064e0781166
Maxime <lathiere.maxime@gmail.com>
2021-03-16 16:32:08 +0100

    test

---
+++ Swiftomatic/testMax.swift
@@ -0,0 +1,20 @@
+//
+//  testMax.swift
+//  Swiftomatic
+//
+//  Created by Maxime Lathiere on 16/03/2021.
+//
+
+import SwiftUI
+
+struct testMax: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct testMax_Previews: PreviewProvider {
+    static var previews: some View {
+        testMax()
+    }
+}
