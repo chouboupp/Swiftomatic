6a3f2e2ede5f54b4b361edd300779014943b75ef
Maxime <lathiere.maxime@gmail.com>
2021-03-16 16:07:34 +0100

    add test2

---
+++ test2.swift
@@ -0,0 +1,20 @@
+//
+//  test2.swift
+//  Swiftomatic
+//
+//  Created by Maxime Lathiere on 16/03/2021.
+//
+
+import SwiftUI
+
+struct test2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct test2_Previews: PreviewProvider {
+    static var previews: some View {
+        test2()
+    }
+}
